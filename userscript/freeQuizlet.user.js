e1e23b4e2439ab14012879aed21436bb39f20f2c
SimbaFs <39305460+simbafs@users.noreply.github.com>
2023-09-20 22:51:26 +0800

    Update freeQuizlet.user.js

--- userscript/freeQuizlet.user.js
+++ userscript/freeQuizlet.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Free Quizlet
 // @namespace    https://simbafs.cc
-// @version      0.1
+// @version      1.0.0
 // @description  免費用 quizlet
 // @author       simbafs
 // @match        https://quizlet.com/explanations/textbook-solutions/*
@@ -10,10 +10,17 @@
 
 (function() {
     'use strict';
+    setTimeout(() => {
+        let a = (new Date()).toISOString()
+        console.log(a)
 
-    document.querySelectorAll('.hideBelow--s').forEach(item => item.remove());
-    document.querySelector('.ExplanationSolutionsContainer').classList.remove('hnqbbas');
+        console.log(document.querySelectorAll('.hideBelow--s'))
+        console.log(document.querySelector('.ExplanationSolutionsContainer'))
+        document.querySelectorAll('.hideBelow--s').forEach(item => item.remove());
+        document.querySelector('.ExplanationSolutionsContainer').classList.remove('hnqbbas');
 
-    console.log('已經把登入牆移掉，如果有任何問題請回報 me@simbafs.cc')
+        console.log('已經把登入牆移掉，如果有任何問題請回報 me@simbafs.cc')
+    }, 2000)
+
     // Your code here...
 })();
